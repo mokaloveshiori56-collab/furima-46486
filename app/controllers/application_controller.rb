4b4cafee9d76ceec77ea16b6a7a151a699e2bfb1
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-18 18:39:12 +0900

    スタイル修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,21 @@ class ApplicationController < ActionController::Base
   # production環境でもBasic認証が必要なら if Rails.env.production? に変更
   before_action :basic_auth unless Rails.env.test?
 
+  # Deviseのパラメーターを許可するためのメソッドを呼び出す
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
+  end
+
+  def configure_permitted_parameters
+    # 新規登録時に追加したカラムを許可する
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 
   # Deviseのログイン・ログアウト後のリダイレクト先 (既存のものであれば変更不要)
