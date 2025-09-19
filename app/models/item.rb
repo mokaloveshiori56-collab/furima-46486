ce0a647867ae88e1e5d5e2b61d5c8ba237fbc684
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-19 19:27:24 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,21 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
+with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
+  end
+
+  # ActiveHashのIDに対するバリデーション
+  # '---'が選択されたままでは保存できないようにする
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :item_condition_id
+    validates :shipping_fee_burden_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
 end
+
