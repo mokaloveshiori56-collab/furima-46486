d2caeaee35ac4a81a6c66e6d248147c4a501a387
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-19 21:13:34 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
+  end
+
+  def new
+    # 商品出品フォームで使うためのItemモデルのインスタンスを生成
+    @item = Item.new
   end
 end
