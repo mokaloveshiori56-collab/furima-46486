2970beedf3f775d984c7e9e798e8f1ac9b41921f
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-19 22:20:26 +0900

    単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
   factory :item do
-    item_name { "MyString" }
-    item_description { "MyText" }
-    category_id { 1 }
-    item_condition_id { 1 }
-    shipping_fee_burden_id { 1 }
-    prefecture_id { 1 }
-    days_to_ship_id { 1 }
-    price { 1 }
-    user { nil }
+    association :user # ItemモデルがUserモデルに属することを定義
+    
+    # バリデーションに合う適切な値を設定
+    item_name              { Faker::Commerce.product_name }
+    item_description       { Faker::Lorem.sentence }
+    category_id            { 2 } # 初期値1以外
+    item_condition_id      { 2 } # 初期値1以外
+    shipping_fee_burden_id { 2 } # 初期値1以外
+    prefecture_id          { 2 } # 初期値1以外
+    days_to_ship_id        { 2 } # 初期値1以外
+    price                  { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    # テスト用の画像を添付
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
