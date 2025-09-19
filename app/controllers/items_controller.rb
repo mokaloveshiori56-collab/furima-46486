045965f42f0e72a098985ea2cda3f940540fc1c4
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-19 21:21:42 +0900

    商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
@@ -8,4 +8,22 @@ class ItemsController < ApplicationController
     # 商品出品フォームで使うためのItemモデルのインスタンスを生成
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました'
+    else
+      render :new
+    end
+  end
+   private
+
+  def item_params
+    params.require(:item).permit(
+      :item_name, :item_description, :category_id, :item_condition_id,
+      :shipping_fee_burden_id, :prefecture_id, :days_to_ship_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
 end
+
