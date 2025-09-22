2cfbb251c385e96510eb737bf95efc37ee37ba54
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-22 18:48:14 +0900

    商品情報の一覧表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,11 +21,11 @@ class ApplicationController < ActionController::Base
   end
 
   # Deviseのログイン・ログアウト後のリダイレクト先 (既存のものであれば変更不要)
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     root_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     root_path
   end
 end
