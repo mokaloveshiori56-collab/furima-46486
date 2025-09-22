46900016b2e6ea8fba94bf891a26f6ddb256a020
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-22 16:37:45 +0900

    修正

--- db/migrate/20250919092530_create_items.rb
+++ db/migrate/20250919092530_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      t.string :item_name
-      t.text :item_description
-      t.integer :category_id
-      t.integer :item_condition_id
-      t.integer :shipping_fee_burden_id
-      t.integer :prefecture_id
-      t.integer :days_to_ship_id
-      t.integer :price
+      t.string :item_name, null: false
+      t.text :item_description, null: false
+      t.integer :category_id, null: false
+      t.integer :item_condition_id, null: false
+      t.integer :shipping_fee_burden_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :days_to_ship_id, null: false
+      t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品が出品できない場合' do
+      it 'ユーザーが紐づいていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+
       it '商品画像が空では出品できない' do
         @item.image = nil
         @item.valid?
@@ -30,7 +36,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Item description can't be blank")
       end
-      
+
       # ActiveHashのテスト
       it 'カテゴリーが「---」では出品できない' do
         @item.category_id = 1
@@ -66,25 +72,25 @@ RSpec.describe Item, type: :model do
       it '価格が空では出品できない' do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number")
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
       end
 
       it '価格が300円未満では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it '価格が10,000,000円以上では出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
       it '価格が半角数値でなければ出品できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
