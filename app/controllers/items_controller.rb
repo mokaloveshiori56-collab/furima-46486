17e9f7467a40629df64d4ea4a8c4cfc6887074f8
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-23 17:40:48 +0900

    出品者以外は編集ページに遷移できないように制御

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,6 +38,16 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    return if @item.user == current_user
+
+    redirect_to root_path
+  end
 
   def item_params
     params.require(:item).permit(
