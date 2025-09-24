cfbca335bd4ac5a34f7bd6689234089234f1dd39
mokaloveshiori56-collab <mokaloveshiori56@gmail.com>
2025-09-24 17:59:43 +0900

    モデルやコントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :token).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
+end
